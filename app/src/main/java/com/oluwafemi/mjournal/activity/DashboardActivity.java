f577176b8be8029cd5f534b71b670fab13a5db26
Oluwafemi Bamisaye <oluwafemi.bamisaye@techadvance.ng>
2018-06-27 19:07:26 +0100

    google sign in added

---
+++ app/src/main/java/com/oluwafemi/mjournal/activity/DashboardActivity.java
@@ -0,0 +1,15 @@
+package com.oluwafemi.mjournal.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    private static final String TAG = "DashboardActivity";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
